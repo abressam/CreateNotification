a96a5586df70edad6cc39266326dde966f0f6484
Amanda Bressam <amandabressam@gmail.com>
2022-02-17 16:45:11 -0300

    Updated main activity to create the permission

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,26 +1,64 @@
 package com.example.notification
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val notificationBtn = findViewById<Button>(R.id.show_notification_btn)
         notificationBtn.setOnClickListener { showNotification() }
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Toast.makeText(this, "Permission received", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        val permissionBtn = findViewById<Button>(R.id.permission_button)
+        permissionBtn.setOnClickListener { requestPermission(requestPermissionLauncher) }
     }
+
+    private fun requestPermission(requestPermission: ActivityResultLauncher<String>) = when {
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED -> {
+            // You can use the API that requires the permission.
+        }
+        shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+            Toast.makeText(this, "Permission is needed for this and that", Toast.LENGTH_SHORT).show()
+        }
+        else -> {
+            requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+    }
+
 
     private fun showNotification() {
         val intent = Intent(this, NotificationActivity::class.java).apply {
